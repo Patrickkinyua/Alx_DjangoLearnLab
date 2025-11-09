6329b84af84e3bba467a852b21aafd46c4fbdc42
patrick_mugo <patrickkinyua2004@gmail.com>
2025-11-09 21:55:16 +0300

    typo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
+from .models import Library
 
 # ✅ Function-based view: list all books
 def list_books(request):
