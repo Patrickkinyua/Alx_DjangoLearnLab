193812e044351703beb64c89fcf2450e441d0b1e
patrick_mugo <patrickkinyua2004@gmail.com>
2025-11-09 21:56:14 +0300

    2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 from .models import Library
+from django.views.generic.detail import DetailView
 
 # ✅ Function-based view: list all books
 def list_books(request):
